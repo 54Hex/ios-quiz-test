ca732f2373fd20bc2e852c8e419c7ffe8f76aae6
Xuyuan <t.xuyuan@gmail.com>
2021-06-12 12:16:44 +0800

    Added appropriately sized outlines around options

--- Quiz/Question.swift
+++ Quiz/Question.swift
@@ -15,10 +15,18 @@ struct Question {
     var option2: String
     var option3: String
     var option4: String
-    var ans: Int
+    var answer: Int
     
-    var lengths:Array<Int> = [0]
-    
-    
+    var maxLength: Int
+    init(ques:String, opt1: String, opt2:String, opt3: String, opt4: String, ans: Int){
+        title = ques
+        option1 = opt1
+        option2 = opt2
+        option3 = opt3
+        option4 = opt4
+        answer = ans
+        let lengths: Array<Int> = [option1.count, option2.count, option3.count, option4.count]
+        maxLength = lengths.max()! * 15
+    }
     
 }
